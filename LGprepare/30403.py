73352e5369fb302f879a11cdaf0e3ea0265cd252
viroovr <tjgusdnjs789@gmail.com>
2023-11-01 00:24:26 +0900

    무지개 만들기

---
+++ LGprepare/30403.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+rainbow = 'roygbiv'
+def main():
+    N = int(input())
+    string = input()
+    dic = defaultdict(int)
+    for s in string:
+        dic[s] += 1
+    lower_rainbow = False
+    for s in rainbow:
+        if dic[s] == 0:
+            break
+    else:
+        lower_rainbow = True
+    upper_rainbow = False
+    for s in rainbow.upper():
+        if dic[s] == 0:
+            break
+    else:
+        upper_rainbow = True
+    if lower_rainbow and upper_rainbow:
+        print("YeS")
+    elif not lower_rainbow and not upper_rainbow:
+        print('NO!')
+    elif not upper_rainbow:
+        print("yes")
+    else:
+        print("YES")
+    
+main()
