253c481122cb6d3662dfb1695469181187ec533a
viroovr <tjgusdnjs789@gmail.com>
2024-04-27 20:54:30 +0900

    2xn 타일링 2 [11727]

---
+++ 10000/11727.py
@@ -0,0 +1,16 @@
+DIV = 10_007
+n = int(input())
+tiling = [-1] * 1001
+tiling[0] = 0
+tiling[1] = 1
+tiling[2] = 3
+
+
+def count(n):
+    if tiling[n] != -1:
+        return tiling[n]
+    tiling[n] = (count(n - 1) + 2 * count(n - 2)) % DIV
+    return tiling[n]
+
+
+print(count(n))
